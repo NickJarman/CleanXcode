2731c2c7513d933603f2ee6937de4eca2220075f
Nick Jarman <nick@nickjarman.com>
2021-11-01 13:12:23 +0000

    Added script and brief documentation in README.md.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# clean-xcode
-Simple command line tool to minimise space used by Xcode.
+# CleanXcode
+A simple command line tool to minimise space used by Xcode. It's based on the script in [this article]( https://www.raywenderlich.com/19998365-understanding-and-managing-xcode-space) by Keegan Rush at Ray Wenderlich, with a few improvements:
+
+- Keeps the two most recent iOS versions and the most recent watchOS and tvOS versions.
+- Moves directories to the trash rather than deleting them with rm -rf, which is safer, can be undone, and shows what has been removed.
+
+No parameters needed, just...
+
+`./clean-xcode`
+
+...after updating your iOS device and attaching it to Xcode.
---
+++ clean-xcode.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+function CleanXcodeDirectory() {
+	if [ -d $1 ]; then
+		echo "Cleaning $1";
+		Filenames=()
+	
+		# Get directories in numeric order using (/n): / = directories, n = numeric sort
+		Filenames=("$1"/*(/n))
+		FileCount=${#Filenames[@]}
+	
+		echo Directories: $FileCount
+	
+		if [ $FileCount -gt $2 ]; then
+			let "CountToDelete = $FileCount - $2"
+
+			echo Directories to delete: $CountToDelete
+
+			# Directory names are in numeric order so we can just delete <count> - <number-to-keep> directories, knowing that the ones which are not deleted are for the most recent OS releases.
+			for ((i = 1; i <= $CountToDelete; i++)); do
+				osascript -e "tell application \"Finder\" to delete POSIX file \"${Filenames[$i]}\""
+			done
+		else
+			echo No items to delete.
+		fi
+	fi
+}
+
+# Clean Xcode directories - first parameter is the path, second parameter is how many subdirectories to keep.
+CleanXcodeDirectory ~/Library/Developer/Xcode/iOS\ DeviceSupport 2
+CleanXcodeDirectory ~/Library/Developer/Xcode/watchOS\ DeviceSupport 1
+CleanXcodeDirectory ~/Library/Developer/Xcode/tvOS\ DeviceSupport 1
+
+# Remove unavailable simulators
+xcrun simctl delete unavailable
